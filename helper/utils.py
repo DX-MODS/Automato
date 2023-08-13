9df234c2fa119644a72b0c115a7e916d4a41730c
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-08-14 00:22:52 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -9,9 +9,28 @@ from datetime import datetime
 from pytz import timezone
 from config import Config 
 from pyrogram import Client
+from pyrogram.types import Message
 HELP = {}
 CMD_HELP = {}
 Automato = Client
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
 def humanbytes(size):    
     if not size:
         return ""
