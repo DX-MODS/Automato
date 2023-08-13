35f32923d7b2844fcca9a8cc4873cf67855aa04a
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-08-13 23:04:53 +0530

    Update paste.py

--- plugins/paste.py
+++ plugins/paste.py
@@ -7,8 +7,8 @@ from helper.utils import Automato
 
 
 @Automato.on_message(filters.private & filters.command("neko"))
-async def neko(_, message: Message):
-    text = message.reply_to_message.text
+async def neko(client, message):
+    text = client.reply_to_client.text
     try:
         params = {"content": text}
         headers = {'content-type' : 'application/json'}
@@ -16,30 +16,30 @@ async def neko(_, message: Message):
         neko = requests.post(url, json=params, headers=headers)
         key = neko.json()["result"]["key"]
     except Exception:
-        await message.edit_text("`API is down try again later`")
+        await client.edit_text("`API is down try again later`")
         await asyncio.sleep(2)
-        await message.delete()
+        await client.delete()
         return
     else:
         url = f"https://nekobin.com/{key}"
         reply_text = f"**Pasted to: [Nekobin]({url})**"
         delete = (
             True
-            if len(message.command) > 1
-            and message.command[1] in ["d", "del"]
-            and message.reply_to_message.from_user.is_self
+            if len(client.command) > 1
+            and client.command[1] in ["d", "del"]
+            and client.reply_to_client.from_user.is_self
             else False
         )
         if delete:
             await asyncio.gather(
-                Automato.send_message(
-                    message.chat.id, reply_text, disable_web_page_preview=True
+                Automato.send_client(
+                    client.chat.id, reply_text, disable_web_page_preview=True
                 ),
-                message.reply_to_message.delete(),
-                message.delete(),
+                client.reply_to_client.delete(),
+                client.delete(),
             )
         else:
-            await message.edit_text(
+            await client.edit_text(
                 reply_text,
                 disable_web_page_preview=True,
             )
