126c0b166041e6fc5d7dbf2c6d15dd553a523937
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-08-16 13:43:15 +0530

    Update anime.py

--- plugins/anime.py
+++ plugins/anime.py
@@ -10,7 +10,7 @@ API_URL = "https://api.nekosapi.com/v2/images/random"
 @Client.on_message(filters.command(["anime"], PREFIX) & filters.private)
 async def random_anime(client: Client, message: Message):
     # Send the "Processing..." message
-    await message.edit("Fetching a random anime image...")
+    ag = await message.reply_text("Fetching a random anime image...")
 
     # Make the API request
     try:
@@ -20,11 +20,11 @@ async def random_anime(client: Client, message: Message):
         image_url = data["file"]
         title = data["title"]
     except (requests.exceptions.RequestException, KeyError):
-        await message.edit("Failed to fetch a random anime image.")
+        await ag.edit("Failed to fetch a random anime image.")
         return
 
     # Send the image and title as a reply
     await client.send_photo(message.chat.id, image_url, caption=f"**Title:** {title}")
 
     # Edit the original message to indicate success
-    await message.edit("Random anime image sent!")
+    await ag.edit("Random anime image sent!")
