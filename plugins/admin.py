8e477cefb5b8de1f74406225060748dcaaf49d6f
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-08-20 10:06:38 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -2,8 +2,10 @@ from config import Config
 from helper.db import db
 from pyrogram.types import Message
 from pyrogram import Client, filters
+from config import Config
 from helper.utils import Automato
-@Automato.on_message(filters.command("users") & filters.user(Config.ADMIN))
+
+@Automato.on_message(filters.command(["users"], Config.PREFIX) & filters.user(Config.ADMIN))
 async def get_stats(bot :Automato, message: Message):
     mr = await message.reply('**𝙰𝙲𝙲𝙴𝚂𝚂𝙸𝙽𝙶 𝙳𝙴𝚃𝙰𝙸𝙻𝚂.....**')
     total_users = await db.total_users_count()
