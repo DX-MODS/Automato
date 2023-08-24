66338eeff0be63be83df59d8dc42931cf3b1ed7a
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-08-24 19:46:27 +0530

    Update basic.py

--- helper/basic.py
+++ helper/basic.py
@@ -1,5 +1,16 @@
-from pyrogram.types import Message
+from pyrogram import Client, enums
+from pyrogram.types import Message, User
 
+def ReplyCheck(message: Message):
+    reply_id = None
+
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.id
+
+    elif not message.from_user.is_self:
+        reply_id = message.id
+
+    return reply_id
 
 def get_user(message: Message, text: str) -> [int, str, None]:
     """Get User From Message"""
