a45ee37a868bb940c3e82ad4f9e16aa423477ee2
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-08-24 19:52:40 +0530

    Fixed✅

--- helper/basic.py
+++ helper/basic.py
@@ -1,6 +1,23 @@
 from pyrogram import Client, enums
 from pyrogram.types import Message, User
+import asyncio
+import shlex
+from typing import Tuple
 
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+    """Run Commands"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+    
 def ReplyCheck(message: Message):
     reply_id = None
 
